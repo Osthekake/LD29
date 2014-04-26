e7d32d7ad39c0a2f9ac33ee6d019825befcd4e33
Kveldsdraum <ejcorbett@gmail.com>
2014-04-26 19:13:59 +0100

    edit

--- Conversations.js
+++ Conversations.js
@@ -49,12 +49,12 @@ var Conversations = {
 		"start" : "intro01",
 		"screens" : {
 			"intro01" : {
-				"text" : "Walking home one sunny summer day, you take a short-cut through a field.\mThis was a mistake.\mAfter several close encounters with manure, you catch yourself just in time from falling into a deep hole. Who the hell leaves deep holes around the place where people can just fall through them?",
+				"text" : "Walking home one sunny day, you take a short-cut through a field.\nThis is a mistake.\nAfter several close encounters with manure, you catch yourself\njust in time from falling into a deep hole. Who the hell\nleaves deep holes around the place where people can just fall\nthrough them?",
 				"options" : [
 					"coverhole01"]
 			},
 			"intro02" : {
-				"text" : "You find an old door, which looks like it was covering the hole until recently. It's pretty heavy. It has a rope attached to the middle of one side. This does not make it any easier to drag.\mYou take a break from dragging it and sit beside the hole. The smell of cool water comes up from it: it's an old well, not just a mysterious hole. That makes a lot more sense. Still dangerous though.\mYou stand up to drag the door again when you see something shining in the water. It's quite far down.",
+				"text" : "You find an old door, which looks like it was covering the hole\nuntil recently. It's pretty heavy. It has a rope attached to the\nmiddle of one side. This does not make it any easier to drag.\nYou take a break from dragging it and sit beside the hole.\nThe smell of cool water comes up from it: it's an old well,\nnot just a mysterious hole. That makes a lot more sense.\nStill dangerous though.\nYou stand up to drag the door again when you see something\nshining in the water. It's quite far down.",
 				"options" : [
 					"shiny01"]
 			},
@@ -69,12 +69,12 @@ var Conversations = {
 					"shiny03"]
 			},
 			"intro05" : {
-				"text" : "You take firm hold of the rope and start lowering yourself down the well. The door shifts position as your weight drags it towards the well. Then it stops and gets stuck on some rocks which were apparently used to anchor it in place over the well. You reach the end of the rope, your feet touching water, the shiny thing - coins? A sword? A crown? - tantalisingly close but just out of reach...",
+				"text" : "You take firm hold of the rope and start lowering yourself down\nthe well. The door shifts position as your weight drags it\ntowards the well. Then it stops and gets stuck on some rocks\nwhich were apparently used to anchor it in place over the\nwell. You reach the end of the rope, your feet touching water,\nthe shiny thing - coins? A sword? A crown? - tantalisingly\nclose but just out of reach...",
 				"options" : [
 					"badidea01"]
 			},
 			"intro06" : {
-				"text" : "Letting go of the rope is your third bad idea. You drop into the water with a massive splash and soak yourself pretty effectively. At least there's not much water in here. Plus, the shiny thing is just in reach.",
+				"text" : "Letting go of the rope is your third bad idea. You drop into the\nwater with a massive splash and soak yourself pretty\neffectively. At least there's not much water in here. Plus,\nthe shiny thing is just in reach.",
 				"options" : [
 					"shiny04"]
 			},
@@ -84,38 +84,38 @@ var Conversations = {
 					"shiny05"]
 			},
 			"intro08" : {
-				"text" : "It's a lever.\mThe floor drops out from under you. You briefly manage to cling to the lever, saying \"argh\", but you're already tired from dragging that damn door and you fall down, into another pool of water, but that falls as well.",
+				"text" : "It's a lever.\nThe floor drops out from under you. You briefly manage to\ncling to the lever, saying \"argh\", but you're already\ntired from dragging that damn door and you fall down, into\nanother pool of water, but that falls as well.",
 				"options" : [
 					"scream01"]
 			},
 			"intro09" : {
-				"text" : "You seem to be falling for a long time. You keep hitting other pools of water, which then start dropping as well. This is odd.",
+				"text" : "You seem to be falling for a long time. You keep hitting other\npools of water, which then start dropping as well.\nThis is odd.",
 				"options" : [
 					"scream02"]
 			},
 			"intro10" : {
-				"text" : "With an enormous splash, you and what appears to be half of a lake drop into something and stop falling, although it takes you a minute to realise this in your frantic swim to the surface. You drift for a while until your head stops spinning.",
+				"text" : "With an enormous splash, you and what appears to be half of a\nlake drop into something and stop falling, although it takes\nyou a minute to realise this in your frantic swim to the\nsurface. You drift for a while until your head stops spinning.",
 				"options" : [
 					"swim01"]
 			},
 			"intro11" : {
-				"text" : "There's light to see by but it seems to be coming from beneath you rather than above. It's a weird green-blue that you identify as phosphorescence. Eventually. You're not thinking too fast right now.\mThe shore is only twenty metres wide, but there's no sign of another body of water. Actually, there's no sign of anything, which is really weird.",
+				"text" : "There's light to see by but it seems to be coming from beneath\nyou rather than above. It's a weird green-blue that you\nidentify as phosphorescence. Eventually. You're not thinking\ntoo fast right now.\nThe shore is only twenty metres wide, but there's no sign of\nanother body of water. Actually, there's no sign of anything,\nwhich is really weird.",
 				"options" : [
 					"edge01"]
 			},
 			"intro12" : {
-				"text" : "Far down.\mReally far down.\mThere is literally nothing beneath you except spots of phosphorescence. Although they could be your eyes playing tricks with you from all of that down. You back away from the edge and look around for absolutely anything else that could take your mind off all of that down that appears to be waiting for another lever incident.",
+				"text" : "Far down.\nReally far down.\nThere is literally nothing beneath you except spots of phosphorescence.\nAlthough they could be your eyes playing tricks with you from\nall of that down. You back away from the edge and look around\nfor absolutely anything else that could take your mind off all\nof that down that appears to be waiting for another lever\nincident.",
 				"options" : [
 					"clockwise01",
 					"anticlockwise01"]
 			},
 			"intro13" : {
-				"text" : "You resolve never to pull another lever. This resolution fades when you find a bridge concertina'd up a few hundred metres around the lip of what you are growing to suspect is a giant bowl. There's a lever on the side.",
+				"text" : "You resolve never to pull another lever. This resolution fades\nwhen you find a bridge concertina'd up a few hundred\nmetres around the lip of what you are growing to suspect is a\ngiant bowl. There's a lever on the side.",
 				"options" :[
 					"lever01"]
 			},
 			"intro14" : {
-				"text" : "This is your first good idea since you took the short-cut. Well done.",
+				"text" : "This is your first good idea since you took the short-cut.\nWell done.",
 				"options" :[
 				"goforth01"]
 			}
